0a514b0542354f2041b84ebb058a34e44755ccde
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-02-06 19:31:44 +0300

    refactor: final touches

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import { clusterApiUrl, Connection } from '@solana/web3.js';
+import { initializeKeypair } from './helpers';
+
 async function main() {
+  const connection = new Connection(clusterApiUrl('devnet'), 'finalized');
+  const payer = await initializeKeypair(connection);
 }
 
 main()
   .then(() => {
-    console.log("Finished successfully");
+    console.log('Finished successfully');
     process.exit(0);
   })
   .catch((error) => {
