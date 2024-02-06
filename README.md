c89f89829504a1d7a022e533cb4f63c36572c5b5
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-02-06 19:11:05 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# TS Template to run Solana scripts
+
+### Usage
+
+1. hit `yarn`
+1. write your code inside `index.js` main functoin
+1. run the code by running `npm start` or `yarn start`
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+async function main() {
+}
+
+main()
+  .then(() => {
+    console.log("Finished successfully");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
