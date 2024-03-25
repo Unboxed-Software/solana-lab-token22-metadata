0b1a97749a05b8689320d1a74d21f2b168714ac9
Coach Chuck <christian.krueger.health@gmail.com>
2024-03-25 16:24:48 -0600

    working

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,57 +1,6 @@
 import { Connection, Keypair } from '@solana/web3.js';
 import { bundlrStorage, keypairIdentity, Metaplex, toMetaplexFile } from '@metaplex-foundation/js';
 import fs from 'fs';
-import * as web3 from '@solana/web3.js';
-import dotenv from 'dotenv';
-import { getKeypairFromFile } from '@solana-developers/helpers';
-dotenv.config();
-
-async function airdropSolIfNeeded(signer: web3.Keypair, connection: web3.Connection) {
-  const balance = await connection.getBalance(signer.publicKey);
-  console.log('Current balance is', balance / web3.LAMPORTS_PER_SOL);
-
-  if (balance < web3.LAMPORTS_PER_SOL) {
-    console.log('Airdropping 1 SOL...');
-    const airdropSignature = await connection.requestAirdrop(signer.publicKey, web3.LAMPORTS_PER_SOL);
-
-    const latestBlockHash = await connection.getLatestBlockhash();
-
-    await connection.confirmTransaction({
-      blockhash: latestBlockHash.blockhash,
-      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: airdropSignature,
-    });
-
-    const newBalance = await connection.getBalance(signer.publicKey);
-    console.log('New balance is', newBalance / web3.LAMPORTS_PER_SOL);
-  }
-}
-
-export async function initializeKeypair(connection: web3.Connection, keyPairFilePath?: string): Promise<web3.Keypair> {
-  if (keyPairFilePath) {
-    const signer = await getKeypairFromFile(keyPairFilePath);
-
-    await airdropSolIfNeeded(signer, connection);
-
-    return signer;
-  } else if (process.env.PRIVATE_KEY) {
-    const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
-    const secretKey = Uint8Array.from(secret);
-    const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey);
-
-    await airdropSolIfNeeded(keypairFromSecretKey, connection);
-
-    return keypairFromSecretKey;
-  } else {
-    console.log('Creating .env file');
-
-    const signer = web3.Keypair.generate();
-    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
-    await airdropSolIfNeeded(signer, connection);
-
-    return signer;
-  }
-}
 
 export interface CreateNFTInputs {
   payer: Keypair;
