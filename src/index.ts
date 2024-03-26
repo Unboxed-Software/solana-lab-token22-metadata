c027e373ec661a81df7c2103e21c0875a26f2563
Coach Chuck <christian.krueger.health@gmail.com>
2024-03-26 12:48:40 -0600

    working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
 import { clusterApiUrl, Connection } from '@solana/web3.js';
 import { initializeKeypair } from '@solana-developers/helpers';
+import createNFTWithEmbeddedMetadata from './nft-with-embedded-metadata';
+import { uploadOffChainMetadata } from './helpers';
 
 async function main() {
   const connection = new Connection(clusterApiUrl('devnet'), 'finalized');
   const payer = await initializeKeypair(connection);
+
+  const imagePath = 'src/cat.jpg';
+  const tokenName = 'Cat NFT';
+  const tokenDescription = 'This is a cat';
+  const tokenSymbol = 'EMB';
+  const tokenExternalUrl = 'https://solana.com/';
+  const tokenAdditionalMetadata = {
+    species: 'Cat',
+    breed: 'Cool',
+  }
+
+  const tokenUri = await uploadOffChainMetadata({
+    tokenName,
+    tokenDescription,
+    tokenSymbol,
+    imagePath,
+    tokenExternalUrl,
+    tokenAdditionalMetadata,
+  });
+
+  await createNFTWithEmbeddedMetadata({
+    payer,
+    connection,
+    tokenName,
+    tokenSymbol,
+    tokenUri,
+    tokenAdditionalMetadata
+  });
 
 }
 
